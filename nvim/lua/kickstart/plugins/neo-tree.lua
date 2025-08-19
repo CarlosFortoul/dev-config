d78a6b64f254fa4fae66360113b8bd3240c503d4
carlos_fortoul <carlos.fortoul@dell.com>
2025-08-19 18:13:33 -0400

    Updated to only upload necessary config files

---
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -0,0 +1,36 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require("neo-tree").setup({
+     filesystem = {
+      filtered_items = {
+       visible = true, -- show all files by default
+       hide_dotfiles = false,
+       hide_gitignored = false,
+      },
+     },
+  })
+  end,
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
